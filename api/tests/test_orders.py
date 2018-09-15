679c8ed68473ce0f8d8a13a650da296557f119d9
celestemiriams <celestemiriams95@gmail.com>
2018-09-15 12:47:49 +0300

    add more tests

--- api/tests/test_orders.py
+++ api/tests/test_orders.py
@@ -38,9 +38,56 @@ class Tests(TestCase):
         """
         Test case for post an order endpoint, it makes a new order
         """
+
         result = self.client().post('/api/v1/orders/', data=json.dumps(
-            dict(order_id=1, item_category="snacks", item_name="chips",
-                 quantity=2)), content_type='application/json')
+            dict(item_category=False, item_name="chips", quantity=3)),
+                                 content_type='application/json')
+        resp = json.loads(result.data.decode("utf8"))
         self.assertEqual(result.status_code, 400)
+        self.assertIn("data", resp)
+        self.assertIn("error_message", resp)
+        self.assertIsInstance(resp['data'], list)
+        self.assertEqual(resp['error_message'], "some of these fields are missing")
+
+        result = self.client().post('/api/v1/orders/', data=json.dumps(
+            dict(item_category="snacks", item_name="", quantity="")),
+                                 content_type='application/json')
+        resp = json.loads(result.data.decode("utf8"))
+        self.assertEqual(result.status_code, 400)
+        self.assertIn("data", resp)
+        self.assertIn("error_message", resp)
+        self.assertIsInstance(resp['data'], dict)
+        self.assertEqual(resp['error_message'], "some of these fields have empty/no values")
+
+        result = self.client().post('/api/v1/orders/', data=json.dumps(
+            dict(item_category="snacks", item_name="chips", quantity="2")),
+                                 content_type='application/json')
+        resp = json.loads(result.data.decode("utf8"))
+        self.assertEqual(result.status_code, 201)
+        self.assertIn("data", resp)
+        self.assertNotIn("error_message", resp)
+        self.assertIn("success_message", resp)
+        self.assertTrue(resp['data'])
+        self.assertEqual(resp['success_message'], "successfully placed an order.")
+
+    
+    def test_update_order_status(self):
+        """
+        Test case for order endpoint, it tests updates made to an order
+        """
+        result = self.client().put('/api/v1/orders/20/')
+        self.assertEqual(result.status_code, 200)
+
+        result = self.client().put('/api/v1/orders/@/')
+        self.assertEqual(result.status_code, 404)
+
+        result = self.client().put('/api/v1/orders/1/', data=json.dumps(
+            dict(item_category="snacks", item_name="chips", quantity=2)),
+                                content_type='application/json')
+        resp = json.loads(result.data.decode("utf8"))
+        self.assertEqual(result.status_code, 201)
+        self.assertIn("success_message", resp)
+
+
 
 
