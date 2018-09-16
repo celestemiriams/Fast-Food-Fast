9a00772da9b73177d132a1eabfc9499e2c4c1e03
celestemiriams <celestemiriams95@gmail.com>
2018-09-16 05:23:32 +0300

    solve linting issues

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -2,9 +2,8 @@
 app root of the api endpoints
 """
 
-
-from api.instances.config import DevelopmentConfig, ProductionConfig
 from flask import Flask
+from api.instances.config import DevelopmentConfig, ProductionConfig
 from api.app.views import Urls
 
 
@@ -13,7 +12,9 @@ class Server:
 
     @staticmethod
     def create_app(config=None):
-        app = Flask(__name__) 
+        """ This function returns the app"""
+
+        app = Flask(__name__)
         app.config.update(config.__dict__ or {})
         Urls.generate(app)
         return app
